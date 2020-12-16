e0099074649d26ca153d315a3f8c70a36b03cf77
Thiago Bueno Costa <th.bueno.95@gmail.com>
2020-12-16 11:16:00 -0300

    first commit

---
+++ src/main/java/com/conceitosspring/ConceitosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.conceitosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceitosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceitosSpringApplication.class, args);
+	}
+
+}
